61d4ed381416a0eee04756f20c5d82a1629750c1
cristiano <cekeaudio@gmail.com>
2024-10-08 20:13:12 -0300

    creating benefits screen

---
+++ src/components/Service.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+    imgSrc: String,
+    title: String,
+    text: String
+})
+</script>
+
+<template>
+    <div class="container-Serv">
+        <div class="content-Icon">
+            <img :src="imgSrc" alt="Icon">
+        </div>
+        <div class="content-Title">
+            <h2> {{ title }}</h2>
+        </div>
+        <div class="content-txt">
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container-Serv {
+    width: 20rem;
+    height: 24.733;
+    display: flex;
+    justify-content: flex-start;
+    align-items: start;
+    flex-direction: column;
+    gap: .5rem;
+    padding: 1.5rem;
+    border: 1px solid #3A3B3E;
+    border-radius: .9rem;
+}
+.container-Serv h2 {
+    color: #FFFFFF;
+    font-family: 'poppins';
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 2.25rem;
+}
+
+.container-Serv p {
+    color: #FFFFFF;
+    font-family: 'poppins';
+    font-weight: 300;
+    font-size: 1rem;
+    line-height: 2.25rem;
+}
+
+
+</style>
--- src/views/Beneficios.vue
+++ src/views/Beneficios.vue
@@ -1,21 +1,86 @@
 <script setup>
-import HeroSectionTwo from '@/components/HeroSectionTwo.vue';
+import Service from '@/components/Service.vue';
 
 
 </script>
 
 <template>
-    <div class="titl">
-        <h1> Beneficios </h1>
+    <div class="title">
+        <div class="container">
+            <div class="DivTitle">
+                <h1> Nossos Parceiros </h1>
+            </div>
+            <div class="Brand">
+                <Service
+                    imgSrc="src/assets/Luz.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+                <Service
+                    imgSrc="src/assets/medala.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+                <Service
+                    imgSrc="src/assets/adv.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+            </div>
+
+            <div class="Brand">
+                <Service
+                    imgSrc="src/assets/DocBran.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+                <Service
+                    imgSrc="src/assets/BranPC.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+                <Service
+                    imgSrc="src/assets/BrandVoice.svg"
+                    title="Titulo"
+                    text="eiusmod temporm dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporm dolor sit"
+                />
+
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.titl {
-    padding-top: 4.8rem;
+
+.DivTitle {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.titl h1 {
+.Brand {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 2rem;
+}
+
+.title {
+    padding: 4.8rem 0 2.8rem 0;
+}
+
+.title h1 {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 700;
+    font-size: 2.9rem;
+    align-items: center;
     color: #FFFFFF;
 }
 </style>
