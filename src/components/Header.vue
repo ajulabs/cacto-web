0eabb58c98fc842640c862354b3dd65fd896f8fd
cristiano <cekeaudio@gmail.com>
2024-10-31 22:27:19 -0300

    Implement responsive header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -66,46 +66,64 @@ const handleMenuClick = () => {
         color: #FF6400;
     }
     nav {
+        width: 100%;
         display: flex;
         justify-content: space-evenly;
         align-items: center;
     }
-
     .logo {
         width: 100%;
         max-width: 126.6px;
         height: 36.64px;
     }
-
     .menu-ic {
         display: none;
         position: relative;
         z-index: 1000;
         color: #FF6400;
     }
-
-
 /* Responsividade */
+    @media (max-width: 768px) {
+    nav {
+        max-width: 768px;
+        padding: 1rem;
+    }
+    .ul {
+        justify-content: space-evenly;
+    }
+    }
+
+    @media (max-width: 720px) {
+        .content {
+            max-height: 68px;
+        }
+        nav {
+            padding: 0 4rem;
+        }
+    }
+
     @media(max-width:575px){
 
     .content {
         max-width: 575vw;
-        max-height: 88px;
+        max-height: 68px;
         padding: 0;
     }
 
     nav {
         max-width: 575px;
-        padding: 4rem;
+        padding: 2rem;
         justify-content: flex-start;
     }
 
     .logo {
+        width: 100%;
         max-width: 130px;
         max-height: 50px;
     }
 
     .logo img {
+        width: 100%;
         max-width: 575px;
     }
 
@@ -120,67 +138,91 @@ const handleMenuClick = () => {
     }
     }
 
-    /* Smartphones pequenos */
-    @media(max-width: 360px){
+    @media (max-width: 500px){
+    .logo {
+        max-width: 110px;
+    }
+    }
 
-.content {
-    max-width: 575vw;
-    max-height: 88px;
-    padding: 0;
-}
+    @media (max-width: 480px ) {
+    .content {
+        max-height: 58px;
+    }
+    .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .logo img {
+        max-width: 100%;
+    }
+    }
 
-nav {
-    max-width: 575px;
-    padding: 4rem;
-    justify-content: flex-start;
-    align-items: center;
-}
+    @media (max-width: 455px ) {
+    nav {
+        padding: 1rem;
+    }
+    }
 
-.logo {
-    max-width: 107px;
-    max-height: 50px;
-    width: 6rem;
-    padding: 0.3rem ;
-    margin-left: 1rem;
-    height: auto;
-}
+    @media(max-width: 425px){
+    .content {  
+        max-width: 575vw;
+        max-height: 48px;
+        padding: 0;
+    }
 
-.logo img {
-    width: 50%;
-    height: auto;
-}
+    nav {
+        max-width: 375px;
+        justify-content: space-between;
+        align-items: center;
+        padding-left: 6rem;
+    }
 
-.menu-ic {
-    display: block;
-    font-size: 0.5em;
-}
+    .logo {
+        max-width: 120px;
+    }
 
-.ul {
-    position: fixed;
-    top: 0%;
-    left: 62%;
-    width: 50%;
-    height: 100%;
-    padding-left: 4%;
-    padding-top: 1rem;
-    background: #1A1A1A;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    transform:translateX(100%);
-    transition: 0.3s ease;
-}
+    .logo img {
+        width: 100%;
+    }
 
-.ul-open {
-    transform: translateX(0);
-}
+    .menu-ic {
+        display: block;
+        font-size: 2.5em;
+    }
 
-.ul li {
-    padding-top: 0.2rem;
-    margin-top: 1.3rem;
-    font-size: 0.5rem;
-}
+    .ul {
+        position: fixed;
+        top: 0%;
+        left: 62%;
+        width: 100%;
+        height: 100%;
+        padding-left: 4%;
+        padding-top: 1rem;
+        background: #1A1A1A;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: flex-start;
+        transform:translateX(100%);
+        transition: 0.3s ease;
+    }   
 
-}
+    .ul-open {
+        transform: translateX(0);
+    }
+
+    .ul li {
+        padding-top: 0.2rem;
+        margin-top: 1.3rem;
+        font-size: 0.5rem;
+    }
+    }
+    
+    @media (max-width: 384px) {
+        .menu-ic {
+            margin-right: 4rem;
+            margin-left: 2rem;
+        }
+    }
 
 </style>
